2b9069d703cfd428f18c8bf6a26b23e7da071a10
jmassa507 <99853128+jmassa507@users.noreply.github.com>
2022-03-06 23:38:09 -0500

    Add files via upload

---
+++ Cafeteria XYZ.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+
+/*Tarea grupal: Cafeteria XYZ*/
+/*Integrantes: Juan Massa, David Lopez, Israel Castillo, Luis Zhang, Saul Rojas*/
+using namespace std;
+int main() 
+
+{
+	char puerta;
+		cout<<"\nBienvenido a la Cafetria XYZ.\nPresione \na - para abrir la puerta\n";
+		cin>>puerta;
+		
+		if (puerta==('a'))	
+		{ 
+		/*El resto del programa*/
+		}
+			else
+			{
+			cout<<"\nBoton incorrecto, si desea entrar presione \"a\"";
+			}
+}
