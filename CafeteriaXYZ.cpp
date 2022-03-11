13cc37669abc3f5e9365f68e7d50d37d6a99fc2a
Luis E. Zhang <99779394+LuisEZhang@users.noreply.github.com>
2022-03-11 01:54:28 -0500

    Update CafeteriaXYZ.cpp
    Temporizador arreglado y menú modificado y arreglado para que fusioné con el temporizador

--- CafeteriaXYZ.cpp
+++ CafeteriaXYZ.cpp
@@ -43,37 +43,61 @@ int main(){
 			cout<<"\nBoton incorrecto, si desea entrar presione \"a\"";
             goto Inicio;
 		}
+	do{
+    m:
+	printf("******MENU DEL DIA******\n");
+	printf("1.-PLATO DEL DIA\n");
+	printf("2.-RECOMENDACION DEL CHEF\n");
+	printf("3.-PIZZA NAPOLITANA\n");
+    cin>>eleccion;
+	
+    switch(eleccion){
+         case 1: 
+	    printf("El tiempo de espera para este platillo es de 3 minutos desea ordenar este platillo? \nSi->1 \nNo->2\n");
+        cin>>o;
+        if (o==1){
+            hr=00, min=02, sec=60;
+            goto temporizador;
+        }else if (o==2){
+            system("cls");
+            goto m;
+        }
+        break;
+	         
+	    case 2: 
+	    printf("El tiempo de espera para este platillo es de 3 minutos desea ordenar este platillo? \nSi->1 \nNo->2\n");
+        cin>>o;
+        if (o==1){
+            hr=00, min=04, sec=60;
+            goto temporizador;
+        }else if (o==2){
+            system("cls");
+            goto m;
+        } 
+	    break;
+	         
+	    case 3: 
+	    printf("El tiempo de espera para este platillo es de 3 minutos desea ordenar este platillo? \nSi->1 \nNo->2\n");
+        cin>>o;
+        if (o==1){
+            hr=00, min=01, sec=60;
+            goto temporizador;
+        }else if (o==2){
+            system("cls");
+            goto m;
+        }
+	    break;
+
+        default:
+        cout<< "Opcion invalida favor de intentarlo nuevamente"<<endl;
+        goto m;
+        break;
+	    }
             
+   temporizador:
     do{
         system("cls");      
-        cout<< "Mesa numero: "<<m<<" 
-
-        	while(eleccion)
- 	{
-	  printf("******MENU DEL DIA******\n");
-	  printf("NUM1.-PLATO DEL DIA\n");
-	  printf("NUM2.-RECOMENDACION DEL CHEF\n");
-	  printf("NUM3.-PIZZA NAPOLITANA\n");
-	}
-      	switch(eleccion)
-	    {
-         case 1: 
-	         eleccion= num1;
-	         printf("PLATO DEL DIA\n",eleccion); 
-	         break;
-	         
-	         case 2: 
-	             eleccion= num2;
-	             printf("RECOMENDACION DEL CHEF\n",eleccion); 
-	             break;
-	         
-	             case 3: 
-	                 eleccion= num3;
-	                 printf("PIZZA NAPOLITANA\n",eleccion); 
-	                 break;
-	    }
-        
-         "<< "su orden estara lista en: "<<endl;
+        cout<< "Mesa numero: "<<m<<" "<< "su orden estara lista en: "<<endl;
         cout<< hr << ":" << min << ":" << sec <<endl;
         sleep_for(seconds(1));
         sec--;
@@ -91,7 +115,9 @@ int main(){
             sec=59;
         }
 
-    }while (fin==0);
+        }while (fin==0);
+
+    }while (x!=0);
 
 	return 0;
 }
